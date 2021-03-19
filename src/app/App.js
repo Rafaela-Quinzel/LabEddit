54de18104d96dad57c52946a4bdc1a3b909518c5
Rafaela-Quinzel <71155947+Rafaela-Quinzel@users.noreply.github.com>
2021-03-19 14:15:25 -0300

    Pastas e arquivos iniciais

---
+++ src/app/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+
+function App() {
+  window.document.title = "LabEddit"
+  return (
+    <div >
+      
+    </div>
+  )
+}
+
+export default App
