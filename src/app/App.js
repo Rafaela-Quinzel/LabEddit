b91c08145d41496225bd93511f7fc50738408155
Rafaela-Quinzel <71155947+Rafaela-Quinzel@users.noreply.github.com>
2021-03-19 14:32:07 -0300

    Criação das pastas e arquivos referentes as páginas

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from '@material-ui/core/styles'
+import { theme } from '../constants/theme'
 
 function App() {
   window.document.title = "LabEddit"
+  
   return (
-    <div >
-      
-    </div>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
